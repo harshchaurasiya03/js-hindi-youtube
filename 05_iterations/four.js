fdf61eac2a812e47d043485bfcc22f62092eff45
harshchaurasiya03 <120143477+harshchaurasiya03@users.noreply.github.com>
2024-10-14 18:19:23 +0000

    Iterations

---
+++ 05_iterations/four.js
@@ -0,0 +1,31 @@
+//for in loop
+
+const myObject ={
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programing = ["js", "rb", "py", "java", "cpp"]
+
+for(const key in programing){
+    // console.log(programing[key]);   
+}
+
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('USA', 'United State of America')
+// map.set('Fr', 'France')
+
+
+// for (const key in map) {
+//    console.log(key);
+   
+// }
+
